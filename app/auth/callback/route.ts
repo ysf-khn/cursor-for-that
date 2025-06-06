a0cffda75ccf1756fbd42a4552b299f2bdb543d0
Yusuf <yusufmohd72@gmail.com>
2025-06-07 02:47:56 +0530

    auth fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,28 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: NextRequest) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
   if (code) {
     const supabase = await createClient();
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${siteUrl}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${siteUrl}${next}`);
       }
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(`${siteUrl}/auth/auth-code-error`);
 }
